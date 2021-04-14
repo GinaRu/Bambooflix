8626e553d1a03deb95c22706f353bf360f71709e
Gina Rubira <grubirapa@gmail.com>
2021-04-14 14:04:41 +0200

    Profile amb avatar, name i id creat a UserDefaults.

--- Bambooflix/Bambooflix/ProfileEditionVC.swift
+++ Bambooflix/Bambooflix/ProfileEditionVC.swift
@@ -13,14 +13,35 @@ class ProfileEditionVC: UIViewController {
     let profileManager = ProfileManager()
     
     @IBAction func cancelarButton(_ sender: UIBarButtonItem) {
-       
+        if let navController = self.navigationController {
+            navController.popViewController(animated: true)
+        }
     }
     
     @IBAction func guardarButton(_ sender: UIBarButtonItem) {
+        
+        if let navController = self.navigationController {
+            navController.popViewController(animated: true)
+        }
+        
+        if let id = ProfileViewModel.selectedProfileId
+             {
+            var name = nameTexfield.text ?? "Unknown"
+            if name.isEmpty {
+                name = "Añadir"
+            }
+        let profile = Profile(id: id, name: name, imageName: "avatar_20_mini")
+            
+            profileManager.saveProfile(profile)
+        }
+        
+        
+
     }
     
     
     @IBAction func nameTextField(_ sender: Any) {
+        
     }
     
     @IBOutlet var nameTexfield: UITextField!
@@ -28,6 +49,9 @@ class ProfileEditionVC: UIViewController {
     @IBAction func eliminarButton(_ sender: Any) {
         // pop up alert
     }
+    
+    
+    
     
     
 }
--- Bambooflix/Bambooflix/ProfileSelectionVC.swift
+++ Bambooflix/Bambooflix/ProfileSelectionVC.swift
@@ -10,17 +10,53 @@ import UIKit
 
 class ProfileSelectionVC: UIViewController {
     
+    private let profileManager = ProfileManager()
+    private var currentProfiles: [Profile] = []
+    private var isEditModeEnabled: Bool = false
+    
     @IBAction func listoButton(_ sender: UIBarButtonItem) {
+        
     }
     
     @IBAction func editarButton(_ sender: UIBarButtonItem) {
+        
     }
+    
+    
     
     
     @IBAction func añadirPerfilTouched(_ sender: UIButton) {
+        ProfileViewModel.selectedProfileId = sender.tag
+       performSegue(withIdentifier: "segueToEditProfile", sender: nil)
     }
     
     
+    @IBOutlet var buttonsOutlet: [UIButton]!
+    
     @IBOutlet var nombrePerfilLabel: [UILabel]!
     
-}
+    func configureProfileButtons() {
+        let profileList = profileManager.readProfiles()
+        for p in profileList {
+            if p.id == nombrePerfilLabel[p.id].tag {
+                nombrePerfilLabel[p.id].text = p.name
+            }
+            if p.id == buttonsOutlet[p.id].tag {
+                let image = UIImage(named: p.imageName)
+                buttonsOutlet[p.id].setImage(image, for: .normal)
+            }
+ 
+            }
+        }
+    override func viewWillAppear(_ animated: Bool) {
+       configureProfileButtons()
+    }
+    
+    }
+    
+  
+    
+
+
+
+
